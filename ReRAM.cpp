d526eb2703c94946f8aa607a2f22113207f3045e
Fede Ramon <juanfedericoramirezrios@gmail.com>
2026-05-29 22:42:40 -0600

    feat!: vector of RERAM_PULSES objects instead of structures

--- ReRAM.cpp
+++ ReRAM.cpp
@@ -8,62 +8,133 @@ g++ -std=c++20 -I -fPIC -shared -o ReRAM.dll ReRAM.cpp
 #include <numbers>
 #include <random>
 #include <fstream>
+// #include <vector>
 
 #include "CppUtilitiesFede.hpp"
 
 using namespace std;
 
-struct RERAM_LAYER {
-    unsigned ID_layer;
+// struct RERAM_VALS {
+//     unsigned ID_layer;
 
-    int* pulsesPot;
-    float* valsWpot;
-    float* valsWpot_smooth;
+//     int* pulsesPot; // (Ppot_max)
+//     float* valsWpot; // (Ppot_max)
+//     float* valsWpot_smooth; // (Ppot_max)
 
-    int* pulsesDep;
-    float* valsWdep;
-    float* valsWdep_smooth;
+//     int* pulsesDep; // (Pdep_max)
+//     float* valsWdep; // (Pdep_max)
+//     float* valsWdep_smooth; // (Pdep_max)
 
-    // float* W;
-    int rows;
-    int columns;
-};
+//     // float* W;
+//     int rows;
+//     int columns;
+// };
 
 class RERAM_PULSES {
 private:
-vector<RERAM_LAYER> ReRAM_layers;
+    // vector<RERAM_VALS> ReRAM_layers;
+    // unsigned ID_layer;
+
+    int* pulsesPot; // (Ppot_max)
+    float* valsWpot; // (Ppot_max)
+    float* valsWpot_smooth; // (Ppot_max)
+
+    int* pulsesDep; // (Pdep_max)
+    float* valsWdep; // (Pdep_max)
+    float* valsWdep_smooth; // (Pdep_max)
+
+    float* W;
+    int rows;
+    int columns;
 
 public:
-
-
-    RERAM_PULSES() {
+    // RERAM_PULSES() {
         
         
+    // };
+    RERAM_PULSES(
+        // unsigned ID_layer,
+
+        int* pulsesPot, // (Ppot_max)
+        float* valsWpot, // (Ppot_max)
+        float* valsWpot_smooth, // (Ppot_max)
+
+        int* pulsesDep, // (Pdep_max)
+        float* valsWdep, // (Pdep_max)
+        float* valsWdep_smooth, // (Pdep_max)
+
+        int rows,
+        int columns
+    ) {
+        
+        
+    };
+    ~RERAM_PULSES() {
+
     };
     bool Init() {
         return false;
     };
-    void PushReRAMlayer(RERAM_LAYER ReRAMlayer) {
-        ReRAM_layers.push_back(ReRAMlayer);
-    }
-    ~RERAM_PULSES() {
-
-    }
+    // void PushReRAMlayer(RERAM_VALS ReRAMlayer) {
+    //     ReRAM_layers.push_back(ReRAMlayer);
+    // };
 };
 
 
 extern "C" {
-    RERAM_PULSES* obj;
-    void InitReRAMlayers() {
+    vector<RERAM_PULSES*> layers;
+    // RERAM_PULSES* ReRAMlayerObj;
+    // void InitReRAMlayers() {
         
-        obj = new RERAM_PULSES();
+    //     ReRAMlayerObj = new RERAM_PULSES();
+    // }
+    void InitReRAMlayer(
+        // unsigned ID_layer,
+
+        int* pulsesPot, // (Ppot_max)
+        float* valsWpot, // (Ppot_max)
+        float* valsWpot_smooth, // (Ppot_max)
+
+        int* pulsesDep, // (Pdep_max)
+        float* valsWdep, // (Pdep_max)
+        float* valsWdep_smooth, // (Pdep_max)
+
+        int rows,
+        int columns
+    ) {
+        
+        RERAM_PULSES* ReRAMlayerObj = new RERAM_PULSES (
+            // ID_layer,
+
+            pulsesPot, // (Ppot_max)
+            valsWpot, // (Ppot_max)
+            valsWpot_smooth, // (Ppot_max)
+
+            pulsesDep, // (Pdep_max)
+            valsWdep, // (Pdep_max)
+            valsWdep_smooth, // (Pdep_max)
+
+            rows,
+            columns
+        );
+        layers.push_back(ReRAMlayerObj);
+
     }
-    void LoadReRAMlayer(RERAM_LAYER ReRAMlayer) {
-        obj->PushReRAMlayer(ReRAMlayer);
+    void FreeMemory() {
+        for(RERAM_PULSES* layer : layers) {
+            delete(layer);
+        }
+        
+        // delete ReRAMlayerObj;
     }
-    
-    void FreeSimulatorMemory() {
-        delete obj;
+    void LearningReRAMlayer(float* array) {
+
     }
+
+    // void LoadReRAMlayer(RERAM_VALS ReRAMlayer) {
+    //     obj->PushReRAMlayer(ReRAMlayer);
+    // }
 }
+    
+    
 
