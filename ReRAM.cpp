c9f694369474af3660f530de0edb4919661b0e22
Fede Ramon <juanfedericoramirezrios@gmail.com>
2026-06-01 20:55:47 -0600

    feat!: Prototype of LearningReRAMlayer() function in Cpp

--- ReRAM.cpp
+++ ReRAM.cpp
@@ -17,59 +17,73 @@ using namespace std;
 // struct RERAM_VALS {
 //     unsigned ID_layer;
 
-//     int* pulsesPot; // (Ppot_max)
-//     float* valsWpot; // (Ppot_max)
-//     float* valsWpot_smooth; // (Ppot_max)
+//     int* pulsesPot; // (Ppot_max+1)
+//     float* valsWpot; // (Ppot_max+1)
+//     float* valsWpot_smooth; // (Ppot_max+1)
 
-//     int* pulsesDep; // (Pdep_max)
-//     float* valsWdep; // (Pdep_max)
-//     float* valsWdep_smooth; // (Pdep_max)
+//     int* pulsesDep; // (Pdep_max+1)
+//     float* valsWdep; // (Pdep_max+1)
+//     float* valsWdep_smooth; // (Pdep_max+1)
 
 //     // float* W;
 //     int rows;
 //     int columns;
 // };
 
-class RERAM_PULSES {
+class RERAM_LAYER {
 private:
     // vector<RERAM_VALS> ReRAM_layers;
     // unsigned ID_layer;
 
-    int* pulsesPot; // (Ppot_max)
-    float* valsWpot; // (Ppot_max)
-    float* valsWpot_smooth; // (Ppot_max)
+    int Ppot_max;
+    float* valsWpot; // (Ppot_max+1): include experimental noise.
+    float* valsWpot_smooth; // (Ppot_max+1): without experimental noise.
 
-    int* pulsesDep; // (Pdep_max)
-    float* valsWdep; // (Pdep_max)
-    float* valsWdep_smooth; // (Pdep_max)
+    int Pdep_max;
+    float* valsWdep; // (Pdep_max+1)
+    float* valsWdep_smooth; // (Pdep_max+1)
 
-    float* W;
+    float** W;
     int rows;
-    int columns;
+    int cols;
 
 public:
     // RERAM_PULSES() {
         
         
     // };
-    RERAM_PULSES(
+    RERAM_LAYER(
         // unsigned ID_layer,
 
-        int* pulsesPot, // (Ppot_max)
-        float* valsWpot, // (Ppot_max)
-        float* valsWpot_smooth, // (Ppot_max)
+        int Ppot_max,
+        float* valsWpot, // (Ppot_max+1)
+        float* valsWpot_smooth, // (Ppot_max+1)
 
-        int* pulsesDep, // (Pdep_max)
-        float* valsWdep, // (Pdep_max)
-        float* valsWdep_smooth, // (Pdep_max)
+        int Pdep_max,
+        float* valsWdep, // (Pdep_max+1)
+        float* valsWdep_smooth, // (Pdep_max+1)
 
         int rows,
-        int columns
+        int cols
     ) {
-        
+        this->Ppot_max = Ppot_max;
+        this->valsWpot = valsWpot;
+        this->valsWpot_smooth = valsWpot_smooth;
+
+        this->Pdep_max = Pdep_max;
+        this->valsWdep = valsWdep;
+        this->valsWdep_smooth = valsWdep_smooth;
+
+        this->rows = rows;
+        this->cols = cols;
+        for(int row = 0; row < rows; row++)
+            for(int col = 0; col < cols; col++)
+                W[row][col] = 0.0f;
+
+            
         
     };
-    ~RERAM_PULSES() {
+    ~RERAM_LAYER() {
 
     };
     bool Init() {
@@ -82,8 +96,8 @@ public:
 
 
 extern "C" {
-    vector<RERAM_PULSES*> layers;
-    // RERAM_PULSES* ReRAMlayerObj;
+    vector<RERAM_LAYER*> layers;
+    // RERAM_LAYER* ReRAMlayerObj;
     // void InitReRAMlayers() {
         
     //     ReRAMlayerObj = new RERAM_PULSES();
@@ -91,43 +105,51 @@ extern "C" {
     void InitReRAMlayer(
         // unsigned ID_layer,
 
-        int* pulsesPot, // (Ppot_max)
-        float* valsWpot, // (Ppot_max)
-        float* valsWpot_smooth, // (Ppot_max)
+        int Ppot_max,
+        float* valsWpot, // (Ppot_max+1)
+        float* valsWpot_smooth, // (Ppot_max+1)
 
-        int* pulsesDep, // (Pdep_max)
-        float* valsWdep, // (Pdep_max)
-        float* valsWdep_smooth, // (Pdep_max)
+        int Pdep_max,
+        float* valsWdep, // (Pdep_max+1)
+        float* valsWdep_smooth, // (Pdep_max+1)
 
         int rows,
-        int columns
+        int cols
     ) {
         
-        RERAM_PULSES* ReRAMlayerObj = new RERAM_PULSES (
+        RERAM_LAYER* ReRAMlayerObj = new RERAM_LAYER (
             // ID_layer,
 
-            pulsesPot, // (Ppot_max)
-            valsWpot, // (Ppot_max)
-            valsWpot_smooth, // (Ppot_max)
+            Ppot_max,
+            valsWpot, // (Ppot_max+1)
+            valsWpot_smooth, // (Ppot_max+1)
 
-            pulsesDep, // (Pdep_max)
-            valsWdep, // (Pdep_max)
-            valsWdep_smooth, // (Pdep_max)
+            Pdep_max,
+            valsWdep, // (Pdep_max+1)
+            valsWdep_smooth, // (Pdep_max+1)
 
             rows,
-            columns
+            cols
         );
         layers.push_back(ReRAMlayerObj);
 
     }
     void FreeMemory() {
-        for(RERAM_PULSES* layer : layers) {
+        for(RERAM_LAYER* layer : layers) {
             delete(layer);
         }
-        
+        layers.clear();
+        cout << "Size of vector layers after FreeMemory" << layers.size() << endl;
         // delete ReRAMlayerObj;
     }
-    void LearningReRAMlayer(float* array) {
+    void LearningReRAMlayer(int layerReRAM, float** newWsmooth) {
+        /*
+        newW: (rows, cols)
+        potentiation: (rows, cols), if potentiation = false -> depression.
+        */
+        
+
+       
 
     }
 
